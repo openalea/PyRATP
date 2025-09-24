5787426ecef182a8a6671723758b8a9b0e739ecd
cbozonet <cyril.bozonnet@inrae.fr>
2025-09-24 15:58:16 +0200

    cleaned vtk file view

--- example/view_vtk.py
+++ example/view_vtk.py
@@ -5,21 +5,23 @@ Created on Wed Sep 24 14:23:22 2025
 
 @author: cbozonnet
 """
+
+##### a program to view VTK files from python using PyVista ######
+
 ## INSTALL PYVISTA BEFORE RUNNING THIS CODE
 ## USING in a terminal :  pip install pyvista
 ## within you pyratp environment
 
-# a program to view VTK files
 import pyvista as pv
 
-# Read the VTK file (replace 'your_file.vtk' with your file path)
+# Read the VTK file 
 grid = pv.read('data_PAR_day192hour12.vtk')
 
 # Print available arrays (scalars/vectors)
 print(grid.array_names)
 
-# threshold scalar field to remove non-existing voxels (PAR <0)
-thresholded = grid.threshold(value=-100, scalars='PAR_entity_1', invert=False)
+# threshold scalar field to remove non-existing voxels 
+thresholded = grid.threshold(value=-100, scalars='PAR_entity_1')
 
 # Plot with scalars colored
 plotter = pv.Plotter()
