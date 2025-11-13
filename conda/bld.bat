353c271e5464e900b3c7145c473729806a74b1e1
baugetfa <fabrice.bauget@cirad.fr>
2025-11-13 16:33:25 +0100

    back to compilers with MESON_RSP_THRESHOLD

--- conda/bld.bat
+++ conda/bld.bat
@@ -2,4 +2,5 @@
 call "%ProgramFiles%\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 set "CC=cl"
 set "FC=flang-new"
+set "MESON_RSP_THRESHOLD=320000"
 %python% -m pip install . --no-deps --ignore-installed --no-build-isolation -vv
