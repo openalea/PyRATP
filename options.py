d1a8f56efc3f8dfe228937b79be839371985c34d
pradal <christophe.pradal@cirad.fr>
2025-12-15 11:59:36 +0100

    Remove scons tools

--- options.py
+++
@@ -1,8 +0,0 @@
-#scons parameters file
-#use this file to pass custom parameter to SConstruct script
-
-import sys
-if(sys.platform.startswith('win')):
-    #compiler='msvc'
-    compiler= 'mingw' # by default on windows
-
